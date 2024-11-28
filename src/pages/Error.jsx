ba3071fcf33a10ab496114c162d78445e15e85f2
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-11-28 11:51:01 +0100

    Initialisation du projet

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <p>Oups...</p>
+            <p>La page que vous demandez n'existe pas.</p>
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>"Chaque pixel, chaque ligne de code est une histoire à raconter"</h1>
+        </div>
+    );
+};
+
+export default Portfolio;
