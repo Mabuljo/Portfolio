0880aa73aeb4bc7cf20a1891a66d1d2b1aaa56a1
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-12-03 15:17:59 +0100

    style du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,18 @@ import logo from "../assets/images/logo.webp"
 const Header = () => {
     return (
         <header>
-            <div>
+            <div className='header-content'>
                 <h1>"Chaque pixel, chaque ligne de code est une histoire à raconter"</h1>
                 <span><a href="#main"><i className="fa-solid fa-arrow-down"></i></a></span>
             </div>
-            <div>
-                <img src={logo} alt="logo" />
-                <nav>
-                    <a href="#presentation">Présentation</a>
-                    <a href="#competences">Compétences</a>
-                    <a href="#projets">Projets</a>
-                    <a href="#contact">Contact</a>
-                    <a href="/">Mon C V</a>
-                </nav>
-            </div>
+            <nav>
+                <img src={logo} alt="logo" className='nav_logo' />
+                <a href="#presentation">Présentation</a>
+                <a href="#competences">Compétences</a>
+                <a href="#projets">Projets</a>
+                <a href="#contact">Contact</a>
+                <a href="/">Mon C V</a>
+            </nav>
         </header>
     );
 };
