bdf3a570a9aee7f6af8967dac9a5eb3d642a0096
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-12-06 10:24:10 +0100

    Creation du component Form.jsx, ajout du fichierscss mixins

---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from './Button'; 
+
+const Form = () => {
+    return (
+        <div className='contact'>
+                        <p>N'hésitez pas à me laisser un message</p>
+                        <form action="#" method="post">
+                            <div className='form_container'>
+                                <div className='form_container--details'>
+                                    <label htmlFor="nom">Nom</label>
+                                    <input type="text" name="nom" id="nom" required/>
+                                    <label htmlFor="prenom">Prénom</label>
+                                    <input type="text" name="prenom" id="prenom" required/>
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" name="email" id="email" required/>
+                                </div>
+                                <div className='form_container--message'>
+                                    <label htmlFor="message">Votre message</label>
+                                    <textarea name="message" id="message" cols="30" rows="15" required></textarea>
+                                </div>
+                            </div>
+                            <Button type='submit' text="Envoyer" />
+                        </form>
+                    </div>
+    );
+};
+
+export default Form;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,6 +5,7 @@ import Button from '../components/Button';
 import Icon from '../components/Icon';
 import Projets from "../datas/projets.json";
 import Card from '../components/Card';
+import Form from '../components/Form';
 import Footer from '../components/Footer';
 
 
@@ -84,27 +85,8 @@ const Portfolio = () => {
                 </aside>
                  
                 <section id='contact'>
-                    <h2>Me contacter</h2>
-                    <div className='contact'>
-                        <p>N'hésitez pas à me laisser un message</p>
-                        <form action="#" method="post">
-                            <div className='form_container'>
-                                <div className='form_container--details'>
-                                    <label htmlFor="nom">Nom</label>
-                                    <input type="text" name="nom" id="nom" required/>
-                                    <label htmlFor="prenom">Prénom</label>
-                                    <input type="text" name="prenom" id="prenom" required/>
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" name="email" id="email" required/>
-                                </div>
-                                <div className='form_container--message'>
-                                    <label htmlFor="message">Votre message</label>
-                                    <textarea name="message" id="message" cols="30" rows="15" required></textarea>
-                                </div>
-                            </div>
-                            <Button type='submit' text="Envoyer" />
-                        </form>
-                    </div>
+                    <h2>Me contacter</h2>   
+                    <Form />
                  </section>
             </main>
             <Footer />
