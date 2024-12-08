588ff9a5d66104c0c5da8630c6fb3aad60dfd3ce
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-12-08 16:54:15 +0100

    corrections d'accessibilité

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logoFooter from "../assets/images/logo.webp";
 const Footer = () => {
     return (
         <footer>
-            <img src={logoFooter} alt="logo" className='footer_logo' />
+            <img src={logoFooter} alt="logo du portfolio" className='footer_logo' />
             <div className='reseaux'>
                 <a href="https://fr.linkedin.com/" target='_blank' aria-label='Lien LinkedIn'rel="noopener noreferrer" ><i className="fa-brands fa-linkedin"></i></a>
                 <a href="https://github.com/Mabuljo" target='_blank' aria-label='Lien GitHub' rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
