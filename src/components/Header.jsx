7cf78cb352e9410b06ee5dfc2c54d177301a49df
Mabuljo <151923541+Mabuljo@users.noreply.github.com>
2024-12-08 16:01:03 +0100

    Mise en place du style responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from "../assets/images/logo.webp";
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    // Fonction pour l'ajout/retrait de la classe active-menu sur le body
+    useEffect(() => {
+        if (isOpen) {
+            document.body.classList.add('active-menu');
+        } else {
+            document.body.classList.remove('active-menu');
+        }
+    }, [isOpen]); // S'exécute à chaque changement de isOpen
+
+
     return (
         <header>
             <div className='header-content'>
                 <h1>"Chaque pixel, chaque ligne de code est une histoire à raconter"</h1>
-                <span><a href="#main"><i className="fa-solid fa-arrow-down"></i></a></span>
+                <span><a href="#main" aria-label="flèche d'accés"><i className="fa-solid fa-arrow-down"></i></a></span>
             </div>
-            <nav>
+            <nav className={isOpen ? "open" : ""}>
                 <img src={logo} alt="logo" className='nav_logo' />
                 <a href="#presentation">Présentation</a>
                 <a href="#competences">Compétences</a>
@@ -16,6 +33,7 @@ const Header = () => {
                 <a href="#contact">Contact</a>
                 <a href="/">Mon CV</a>
             </nav>
+            <i className={`fa-solid menu ${isOpen ? "fa-xmark" : "fa-bars"}`} onClick={toggleMenu}></i>
         </header>
     );
 };
